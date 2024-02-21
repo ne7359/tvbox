05691676858cc251d1199c0fe7468a1c3ec2498a
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-21 11:03:53 +0800

    Update diy.sh

--- TVBox/takagen99/diy.sh
+++ TVBox/takagen99/diy.sh
@@ -17,8 +17,8 @@ fi
 echo '版本降低至16 安卓4.4
 sed -i '/minSdkVersion/d' TVBoxOSC/app/build.gradle
 sed -i '/com.github.tvbox.osc.tk/a\        minSdkVersion 16' TVBoxOSC/app/build.gradle
-sed -i '/targetSdkVersion/d' TVBoxOSC/app/build.gradle
-sed -i '/minSdkVersion/a\        targetSdkVersion 26' TVBoxOSC/app/build.gradle
+#sed -i '/targetSdkVersion/d' TVBoxOSC/app/build.gradle
+#sed -i '/minSdkVersion/a\        targetSdkVersion 26' TVBoxOSC/app/build.gradle
 
 echo '修改-关于'
 sed -i '/android:text=/d' TVBoxOSC/app/src/main/res/layout/dialog_about.xml
