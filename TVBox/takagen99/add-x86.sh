424419a6edd08aef060734cd551e5677b662de65
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-22 14:19:00 +0800

    Delete TVBox/takagen99/add-x86.sh

--- TVBox/takagen99/add-x86.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "/armeabi-v7a[\'\"]$/s#\$#, 'x86', 'x86_64'#" TVBoxOSC/app/build.gradle
-sed -i "/armeabi-v7a[\'\"]$/s#\$#, 'x86', 'x86_64'#" TVBoxOSC/app/player/build.gradle
-echo '添加x86支持完成'
