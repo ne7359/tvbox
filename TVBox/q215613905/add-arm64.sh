1b3feea251e19dc23b8046fd8ca8c1cd8f007ca0
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-22 14:18:25 +0800

    Delete TVBox/q215613905/add-arm64.sh

--- TVBox/q215613905/add-arm64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "/armeabi-v7a[\'\"]$/s#\$#, 'arm64-v8a'#" TVBoxOSC/app/build.gradle
-sed -i "/armeabi-v7a[\'\"]$/s#\$#, 'arm64-v8a'#" TVBoxOSC/app/player/build.gradle
-echo '添加arm64完成'
