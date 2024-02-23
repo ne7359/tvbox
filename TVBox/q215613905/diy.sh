b6d7a6396b4d377da2e921434a0b74f3c189ba67
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-23 11:50:29 +0800

    Update diy.sh

--- TVBox/q215613905/diy.sh
+++ TVBox/q215613905/diy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "sourceURL=https://github.com/q215613905/TVBoxOS" >> $GITHUB_ENV 
-echo "tag=$(git log --date=format:'%Y.%m.%d-%H.%M' -1 --pretty=format:%cd)" >> $GITHUB_ENV
-# echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
+# echo "tag=$(git log --date=format:'%Y.%m.%d-%H.%M' -1 --pretty=format:%cd)" >> $GITHUB_ENV
+echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
 echo '生成日期完成'
 echo "sourceName=Q" >> $GITHUB_ENV
 
