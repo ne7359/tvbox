e8b20f82170e3f9b61a71a58bb7db56e52db63ad
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-23 19:02:42 +0800

    Update diy.sh

--- TVBox/takagen99/diy.sh
+++ TVBox/takagen99/diy.sh
@@ -4,9 +4,9 @@ echo "sourceURL=https://github.com/takagen99/Box" >> $GITHUB_ENV
 # echo "tag=$(git log --date=format:'%Y.%m.%d-%H.%M' -1 --pretty=format:%cd)" >> $GITHUB_ENV
 # echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
 echo "tag=$(date "+%Y.%m.%d-%H.%M")" >> $GITHUB_ENV   # 添加编译时间
-echo '生成日期完成'
 echo "sourceName=T" >> $GITHUB_ENV
 echo "diy_TIME=$(date "+%Y.%m.%d")" >> $GITHUB_ENV   # 添加版本号变量
+echo '生成日期完成'
 
 touch ./custom.sh
 cat << 'EOF' > ./custom.sh
