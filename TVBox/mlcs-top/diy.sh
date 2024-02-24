5ccacb5f32d54902a1af4523ca64364afe6a0622
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-24 17:06:49 +0800

    Update diy.sh

--- TVBox/mlcs-top/diy.sh
+++ TVBox/mlcs-top/diy.sh
@@ -5,7 +5,7 @@ echo "sourceURL=https://github.com/mlcs-top/iTVBox" >> $GITHUB_ENV
 # echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
 echo "tag=$(date "+%Y.%m.%d-%H.%M")" >> $GITHUB_ENV   # 添加编译时间
 echo "sourceName=ml" >> $GITHUB_ENV
-echo "diy_TIME=$(date "+%Y.%m.%d")" >> $GITHUB_ENV   # 添加版本号变量
+# echo "diy_TIME=$(date "+%Y.%m.%d")" >> $GITHUB_ENV   # 添加版本号变量
 echo '生成日期完成'
 
 touch ./custom.sh
