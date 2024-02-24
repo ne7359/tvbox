0926c92fd54182dbe8be7c4ddbfe431660dcda27
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-24 14:26:49 +0800

    Update diy.sh

--- TVBox/mlabalabala/diy.sh
+++ TVBox/mlabalabala/diy.sh
@@ -4,7 +4,7 @@ echo "sourceURL=https://github.com/mlabalabala/box" >> $GITHUB_ENV
 # echo "tag=$(git log --date=format:'%Y.%m.%d-%H.%M' -1 --pretty=format:%cd)" >> $GITHUB_ENV
 # echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
 echo "tag=$(date "+%Y.%m.%d-%H.%M")" >> $GITHUB_ENV   # 添加编译时间
-echo "sourceName=Q" >> $GITHUB_ENV
+echo "sourceName=M" >> $GITHUB_ENV
 echo "diy_TIME=$(date "+%Y.%m.%d")" >> $GITHUB_ENV   # 添加版本号变量
 echo '生成日期完成'
 
