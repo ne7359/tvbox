08f6c093f3ede9444f42b7ea6b354c9426beb5f8
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-02-25 10:33:38 +0800

    Update diy.sh

--- TVBox/heyin-Box/diy.sh
+++ TVBox/heyin-Box/diy.sh
@@ -1,6 +1,9 @@
+###
+# takagen99 修改版
+###
 #!/bin/bash
 sudo timedatectl set-timezone Asia/Shanghai
-echo "sourceURL=https://github.com/takagen99/Box" >> $GITHUB_ENV 
+echo "sourceURL=https://github.com/HongFeiQ/heyin-Box" >> $GITHUB_ENV 
 # echo "tag=$(git log --date=format:'%Y.%m.%d-%H.%M' -1 --pretty=format:%cd)" >> $GITHUB_ENV
 # echo "tag=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV   # 添加编译时间
 echo "tag=$(date "+%Y.%m.%d-%H.%M")" >> $GITHUB_ENV   # 添加编译时间
