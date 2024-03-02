600bff0228e86bd248a4e8d184e2b0bfc9f2bbca
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-03-02 10:52:24 +0800

    Update diy.sh

--- TVBox/q215613905/diy.sh
+++ TVBox/q215613905/diy.sh
@@ -86,9 +86,6 @@ sed -i 's/color_353744/color_1890FF/g' TVBoxOSC/app/src/main/res/drawable/shape_
 #FongMi的jar支持
 # echo "" >> TVBoxOSC/app/proguard-rules.pro
 # echo "-keep class com.google.gson.**{*;}" >> TVBoxOSC/app/proguard-rules.pro
-
-echo '关于-添加版本号'
-sed -i "/android:text=/s#=\"#=\"版本号: $env.diy_TIME\\\\n\\\\n#" TVBoxOSC/app/src/main/res/layout/dialog_about.xml    # 添加DIY编译时间
 EOF
 chmod +x ./custom.sh
 
